2580517eaf682730fcf9b15b45a7895d01ef4ceb
Sterling Davis <sterlingdayvis@gmail.com>
2022-11-23 19:09:44 -0800

    Update joint_savings.sol

--- Challenge_20/joint_savings.sol
+++ Challenge_20/joint_savings.sol
@@ -21,64 +21,58 @@ pragma solidity ^0.5.0;
 contract JointSavings {
 
     /*
-    Inside the new contract define the following variables:
-    - Two variables of type `address payable` named `accountOne` and `accountTwo`
-    - A variable of type `address public` named `lastToWithdraw`
-    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
+    Define the following variables in the new contract:
+      - Two variables of type `address payable` named `accountOne` and `accountTwo`
+      - A variable of type `address public` named `lastToWithdraw`
+      - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`
     */
-    
     address payable accountOne;
     address payable accountTwo;
     address public lastToWithdraw;
     uint public lastWithdrawAmount;
-    uint public contractBalance; 
+    uint public contractBalance;
 
     /*
-    Define a function named **withdraw** that will accept two arguments.
-    - A `uint` variable named `amount`
-    - A `payable address` named `recipient`
+    Define a function named `withdraw` that accepts two arguments:
+      - An `amount` of type `uint`
+      - A `recipient` of type `payable address`
     */
     function withdraw(uint amount, address payable recipient) public {
 
         /*
-        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        Define a `require` statement that checks if `recipient` is equal to either `accountOne` or `accountTwo`. If it isn’t, the `require` statement returns the “You don't own this account!” text
         */
-        require (recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
 
         /*
-        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        DDefine a `require` statement that checks if `balance` is sufficient for accomplishing the withdrawal operation. If there are insufficient funds, it returns the “Insufficient funds!” text
         */
-        
-        require (amount <= contractBalance, "Insufficient funds!");
+        require(amount <= contractBalance, "Insufficient funds!");
 
         /*
-        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        Add an `if` statement to check if `lastToWithdraw` is not equal to (`!=`) to `recipient`. If it’s not equal, set it to the current value of `recipient`
         */
-        
-        if (lastToWithdraw != recipient){
-            lastToWithdraw = recipient;
+        if (lastToWithdraw != recipient) {
+          lastToWithdraw = recipient;
         }
 
-        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        
+        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument
         recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        
         lastWithdrawAmount = amount;
 
-        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        
-        contractBalance == address(this).balance;
+        // Set the `contractBalance` variable equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract
+        contractBalance = address(this).balance;
     }
 
-    // Define a `public payable` function named `deposit`.
+    // Define a `public payable` function named `deposit`
     function deposit() public payable {
 
         /*
-        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
+        Set the `contractBalance` variable equal to the balance of the contract by using `address(this).balance`
         */
-        contractBalance == address(this).balance;
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -86,18 +80,14 @@ contract JointSavings {
     */
     function setAccounts(address payable account1, address payable account2) public{
 
-        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        
-        account1 = 0x0c0669Cd5e60a6F4b8ce437E4a4A007093D368Cb;
-        account2 = 0x7A1f3dFAa0a4a19844B606CD6e91d693083B12c0;
-        
-        accountOne == account1;
-        accountTwo == account2;
+        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2`, respectively
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
-    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+    Add a fallback function so that your contract can store ether that’s sent from outside the deposit function
     */
-    
-    function () external payable {}
+    function() external payable {}
 }
+
